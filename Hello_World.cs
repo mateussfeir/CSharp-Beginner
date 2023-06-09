84bcb032bc0384a729dae6cf8437d853f7f94c26
Mateus <98242839+mateussfeir@users.noreply.github.com>
2023-06-09 16:48:05 -0400

    Create Hello_World.cs

---
+++ Hello_World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyFirstCSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
